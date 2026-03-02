7b2839cc37831263c68443b6b153184057f95bba
Jayden Davis <jaydendavis746@gmail.com>
2026-03-02 01:25:07 +0000

    Log out

--- salon/settings.py
+++ salon/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = 'service-list'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
