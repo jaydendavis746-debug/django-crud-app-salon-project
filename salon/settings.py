d82482db475bf363a1334b14bd919a78b972ff8d
Jayden Davis <jaydendavis746@gmail.com>
2026-03-06 17:54:45 +0000

    True

--- salon/settings.py
+++ salon/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not 'ON_HEROKU' in os.environ:
+# if not 'ON_HEROKU' in os.environ:
     DEBUG = True
 
 
